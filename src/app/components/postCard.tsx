47bf81bc8b75065f4997b5847db96d541c9fb4d1
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-06-29 17:38:51 +0530

    Add comment post and see option

--- src/app/components/postCard.tsx
+++ src/app/components/postCard.tsx
@@ -1,13 +1,18 @@
 'use client'
 import { createClient } from "@/utils/supabase/client";
+import { Drawer, useMediaQuery } from "@mui/material";
+import moment from "moment";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 export default function PostCard({ post, updating }: { post: any, updating: boolean }) {
   const [loading, setLoading] = useState<boolean>(true)
   const [user, setUser] = useState<any>();
   const [currentUser, setCurrentUser] = useState<any>();
   const [liked, setLiked] = useState<boolean>(false);
+  const [showDrawer, setShowDrawer] = useState<boolean>(false);
+  const [comments, setComments] = useState<any[]>([]);
+  const [commentUser , setCommentUser] = useState<string[]>([]);
   const supabase = createClient();
   async function getLikedStatus() {
     const { data, error } = await supabase.from('postlikes').select('user_id').eq('post_id', post.post_id)
@@ -22,6 +27,8 @@ export default function PostCard({ post, updating }: { post: any, updating: bool
     }
   }
 
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
   useEffect(() => {
     async function GetUser() {
       const { data, error } = await supabase.from('users').select('raw_user_meta_data').eq('id', post.user_id)
@@ -35,8 +42,6 @@ export default function PostCard({ post, updating }: { post: any, updating: bool
       const { data: { user } } = await supabase.auth.getUser();
       setCurrentUser(user);
     }
-
-
     getLikedStatus()
     getCurrentUser()
     GetUser()
@@ -72,8 +77,66 @@ export default function PostCard({ post, updating }: { post: any, updating: bool
       setLiked(false)
     }
   }
+
+  async function getComments(){
+    const { data, error } = await supabase.from('comments').select(` post_id , content , users(raw_user_meta_data), replied_to , i_at`).eq('post_id', post.post_id).limit(50)
+    if(error){
+      console.error(error);
+    }
+    setComments(data!);
+  }
+
+  async function handleCommentOpen(){
+    setShowDrawer(true)
+    if(post.comments === 0) return;
+    getComments()
+  }
+
+  async function handleCommentsPost(e: FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const comment = formData.get('comment');
+    const { data, error } = await supabase.from('comments').insert({ post_id: post.post_id, user_id: currentUser.id, content: comment, replied_to: null, i_at: new Date()})
+    if(error){
+      console.error(error);
+    }
+
+    const { error: updateE } = await supabase.from('posts').update({ comments: post.comments + 1 }).eq('post_id', post.post_id)
+    if(updateE){
+      console.error(updateE);
+    }
+    setComments([...comments, data]);
+  }
+
   return (
     <>
+      <Drawer anchor="right" open={showDrawer} onClose={() => setShowDrawer(false)} PaperProps={{
+        sx: {
+          width: isMobile ? '100%' : '50%',
+        }
+      }}>
+        <button onClick={() => setShowDrawer(false)} className="flex flex-row justify-start">
+          <Image src="/comments/close.svg" width={35} height={35} alt="CloseLogo" className=" m-4 hover:bg-slate-200 focus:bg-slate-400 rounded-full transition-all" />
+        </button>
+        <form className="flex flex-col gap-4 p-4" onSubmit={handleCommentsPost}>
+        <input type="text" placeholder="Add a comment..." className="w-full p-4 border-b-2 border-slate-200 outline-none" name="comment"/>
+        <button type="submit" className="bg-slate-800 hover:bg-slate-700 transition-all text-white p-4 rounded-lg">Comment</button>
+        </form>
+        {
+          comments.length === 0 ? <div className="text-center flex justify-center items-center h-screen font-extrabold text-slate-600 text-xl">Nothing here yet...</div>
+          : 
+          <div className="flex flex-col gap-4 p-4">
+          {comments.map((comment) => (
+            <div key={comment.comment_id} className="flex flex-col gap-4 p-4 bg-slate-200 rounded-lg">
+              <p className="text-slate-800 text-lg">{comment.content}</p>
+              <p className="text-slate-600 text-sm font-bold">{comment.users.raw_user_meta_data.name}</p>
+              <p className="text-slate-600 text-sm">{moment(comment.i_at).startOf('hour').fromNow()}</p>
+            </div>
+          ))
+          }
+          </div>
+        }
+      </Drawer>
       {
         loading ? <h1>Loading...</h1>
           :
@@ -82,7 +145,7 @@ export default function PostCard({ post, updating }: { post: any, updating: bool
             <h1 className="text-2xl font-bold">{post.caption}</h1>
             <p className="text-gray-500 text-center w-[300px] md:w-[500px]">{post.content}</p>
             <div className="flex flex-row align-center justify-end gap-4 w-[300px] md:w-[660px] text-lg">
-              <button className="flex flex-row gap-1">
+              <button className="flex flex-row gap-1" onClick={handleCommentOpen}>
                 <p>{post.comments}</p>
                 <Image src="/postCard/comment.svg" width={35} height={35} alt="CommentLogo" />
               </button>
