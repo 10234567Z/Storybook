1b65563181fba60b34fda2eaabe396d80dca2528
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-07-05 19:46:44 +0530

    Fix extra height issue on body-tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`h-screen w-screen`}>
+      <body className={`h-[99vh] w-screen`}>
         {children}
       </body>
     </html>
